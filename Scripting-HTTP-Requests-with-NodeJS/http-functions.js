7015615e4beb99622595012ea0bd49503c3a5213
Sherlaine <sherlaine.lau@gmail.com>
2018-10-01 20:54:13 +0000

    adding the export

---
+++ Scripting-HTTP-Requests-with-NodeJS/http-functions.js
@@ -0,0 +1,33 @@
+    var https = require('https');
+
+function getHTML(options, stuffIWantToDoWithTheHTMLOnceItIsAvailable){
+    https.get(options, function (stream) {
+
+        var allData = "";
+        
+        stream.on('data', function (data) {
+            allData += data.toString();
+        });
+
+        stream.on('end', function () {
+            
+            stuffIWantToDoWithTheHTMLOnceItIsAvailable(allData)
+        });
+
+
+    });
+
+}
+function printHTML(html){
+    console.log('Printing HTML');
+    console.log(html);
+}
+var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step4.html'
+};
+getHTML(requestOptions, printHTML);
+
+module.exports = {getHTML: getHTML 
+};
+
